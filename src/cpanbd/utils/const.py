fb793702fb6ecadda5ed4b327a8f18eb415add52
zscqsmy <1228075512@qq.com>
2025-11-02 12:48:02 +0800

    更新目录

--- src/cpanbd/utils/const.py
+++ src/cpanbd/utils/const.py
@@ -10,13 +10,15 @@ def load_env():
     """加载 .env 配置(优先项目目录, 其次系统目录)"""
 
     # 在当前目录下查找 .env 文件,找到则返回路径, 否则返回空字符串
-    dotenv_path: str = find_dotenv()
+    dotenv_path: str = find_dotenv(usecwd=True)
     if dotenv_path:
+        # print(f"从项目目录加载环境变量: {dotenv_path}")
         load_dotenv(dotenv_path, override=True)
     else:
         # 尝试从系统默认目录加载
         system_env_path = Path.home() / ".env.panbd"
         if system_env_path.exists():
+            # print(f"从系统目录加载环境变量: {system_env_path}")
             load_dotenv(system_env_path, override=True)
         else:
             return
